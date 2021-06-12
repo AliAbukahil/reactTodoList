549835f54bb5848566a7fca7774853a332813635
ali.abukahil <ali.abukahil@gmail.com>
2021-06-12 17:13:07 +0200

    under construction

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./App.scss"
 // Importing Components
 import Form from "./components/Form"
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] =  useState("");
   return (
     <div className="App">
       <header>
-        <h1>My Todo List</h1>
+        <h1>Ali' Todo List</h1>
       </header>
-      <Form/>
+      <Form setInputText={setInputText} />
+      <TodoList inputText={inputText} />
     </div>
   )
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,14 @@
 import React from "react"
 
-const Form = () => {
+const Form = ({setInputText,}) => {
+  // Here I can write Javascript code and functions
+  const inputTextHandler = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  }
      return (
         <form>
-      <input type="text" className="todo-input" />
+      <input onChange={inputTextHandler} type="text" className="todo-input" />
       <button className="todo-button" type="submit">
         <i className="fas fa-plus-square"></i>
       </button>
